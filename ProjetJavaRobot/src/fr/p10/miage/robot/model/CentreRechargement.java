c86839c3370be0e7f940712cd0a0cc974e3ead98
auteba <auteba@miage21a06.dmiage.u-paris10.fr>
2013-11-25 16:52:58 +0100

    Centre de rechargment : Constructeur + Méthodes

--- ProjetJavaRobot/src/fr/p10/miage/robot/model/CentreRechargement.java
+++ ProjetJavaRobot/src/fr/p10/miage/robot/model/CentreRechargement.java
@@ -1,5 +1,54 @@
 package fr.p10.miage.robot.model;
 
-public class CentreRechargement {
+import java.util.ArrayList;
 
+public class CentreRechargement {
+	private ArrayList<Robot> fileAttente;
+	private int max, curr;
+
+	public CentreRechargement(int max, int curr){
+		this.max = 3;
+		this.curr = curr;
+	}
+
+	public synchronized void lancerRechargement(){
+
+	}
+
+	public void collecterInfo(){
+
+	}
+
+	public void exporterInfo(){
+
+	}
+
+	//Un robot est dechargé et on le met dans la file
+	public synchronized void mettreDansFileAttente(){
+		// TODO Auto-generated method stub
+		while(this.curr == this.max){
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		curr ++;
+		notifyAll();
+	}
+
+	//Un robot est complétement rechargé et on libère une place dans la file
+	public synchronized void enleverDansFileAttente(){
+		while(this.curr == 0){
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		this.curr--;
+		notifyAll();
+	}
 }
